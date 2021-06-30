ba1f02ad3680b0408bf2592c7a53b49575af9e2a
NikaJamburia <nikajamburia@gmail.com>
2021-06-30 17:50:28 +0400

    * added new cors origin

--- web-api-spring/src/main/kotlin/ge/wanderer/web/api/spring/configuration/CorsConfiguration.kt
+++ web-api-spring/src/main/kotlin/ge/wanderer/web/api/spring/configuration/CorsConfiguration.kt
@@ -1,24 +1,19 @@
 package ge.wanderer.web.api.spring.configuration
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.PropertySource
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
-@PropertySource("classpath:web-api.properties")
 class CorsConfiguration {
 
     @Bean
-    fun corsConfigurer(
-        @Value("\${allowed-api-clients}") apiClients: Array<String>
-    ): WebMvcConfigurer =
+    fun corsConfigurer(): WebMvcConfigurer =
         object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/api/**").allowedOrigins(*apiClients)
+                registry.addMapping("/api/**").allowedOrigins("*")
             }
         }
 
